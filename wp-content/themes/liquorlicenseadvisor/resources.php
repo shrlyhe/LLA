8fa666294feaecd60a9dbd96733480ae373b222a
shrlyhe <she@wesleyan.edu>
2018-08-10 15:41:09 -0400

    flesh out other pages

--- wp-content/themes/liquorlicenseadvisor/resources.php
+++ wp-content/themes/liquorlicenseadvisor/resources.php
@@ -5,3 +5,47 @@
  */
 
 get_header(); ?>
+
+
+<div class="row">
+	<div class="col-sm-5 search">
+	<form role="search">
+    <div class="search-control">
+        <input type="search" id="site-search" name="q"
+               placeholder="Search Bar"
+               aria-label="Search through site content">
+        <!-- <button>Search</button> -->
+    </div>
+</form>
+</div>
+
+	<div class="col-sm-8 seo-content">
+		<h2>Beginner SEO Content</h2>
+		</div>
+	<div class="col-sm-8 seo-content">	
+		<h2>Beginner SEO Content</h2>
+	</div>
+	<div class="col-sm-8 seo-content">
+		<h2>Beginner SEO Content</h2>
+		</div>
+	<div class="col-sm-8 seo-content">	
+		<h2>Beginner SEO Content</h2>
+	</div>
+
+	<br><br><br>
+
+	<div class="col-sm-8 industry-content">
+		<h2>Industry/Job Specific Content</h2>
+		</div>
+	<div class="col-sm-8 industry-content">	
+		<h2>Industry/Job Specific Content</h2>
+	</div>
+	<div class="col-sm-8 industry-content">
+		<h2>Industry/Job Specific Content</h2>
+		</div>
+	<div class="col-sm-8 industry-content">	
+		<h2>Industry/Job Specific Content</h2>
+	</div>
+
+
+</div>
