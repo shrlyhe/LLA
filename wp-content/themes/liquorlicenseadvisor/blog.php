e9c58029f1021d4e5be58e19e9f77b7d90bbf220
shrlyhe <she@wesleyan.edu>
2018-08-10 15:59:20 -0400

    add blog

---
+++ wp-content/themes/liquorlicenseadvisor/blog.php
@@ -0,0 +1,26 @@
+<?php 
+
+/** Template Name: Blog Page
+ * description: Blog page for LLA
+ */
+
+get_header(); ?>
+
+<div class="col-sm-5 search">
+	<form role="search">
+		<div class="search-control">
+			<input type="search" id="site-search" name="q"
+			placeholder="Search Bar"
+			aria-label="Search through site content">
+			<!-- <button>Search</button> -->
+		</div>
+	</form>
+</div>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+ 
+	<div class="entry">
+		<?php the_content(); ?>
+	</div><!-- entry -->
+<?php endwhile; ?>
+<?php endif; ?>
--- wp-content/themes/liquorlicenseadvisor/resources.php
+++ wp-content/themes/liquorlicenseadvisor/resources.php
@@ -9,25 +9,25 @@ get_header(); ?>
 
 <div class="row">
 	<div class="col-sm-5 search">
-	<form role="search">
-    <div class="search-control">
-        <input type="search" id="site-search" name="q"
-               placeholder="Search Bar"
-               aria-label="Search through site content">
-        <!-- <button>Search</button> -->
-    </div>
-</form>
-</div>
+		<form role="search">
+			<div class="search-control">
+				<input type="search" id="site-search" name="q"
+				placeholder="Search Bar"
+				aria-label="Search through site content">
+				<!-- <button>Search</button> -->
+			</div>
+		</form>
+	</div>
 
 	<div class="col-sm-8 seo-content">
 		<h2>Beginner SEO Content</h2>
-		</div>
+	</div>
 	<div class="col-sm-8 seo-content">	
 		<h2>Beginner SEO Content</h2>
 	</div>
 	<div class="col-sm-8 seo-content">
 		<h2>Beginner SEO Content</h2>
-		</div>
+	</div>
 	<div class="col-sm-8 seo-content">	
 		<h2>Beginner SEO Content</h2>
 	</div>
@@ -36,13 +36,13 @@ get_header(); ?>
 
 	<div class="col-sm-8 industry-content">
 		<h2>Industry/Job Specific Content</h2>
-		</div>
+	</div>
 	<div class="col-sm-8 industry-content">	
 		<h2>Industry/Job Specific Content</h2>
 	</div>
 	<div class="col-sm-8 industry-content">
 		<h2>Industry/Job Specific Content</h2>
-		</div>
+	</div>
 	<div class="col-sm-8 industry-content">	
 		<h2>Industry/Job Specific Content</h2>
 	</div>
