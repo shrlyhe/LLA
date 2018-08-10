2cfb6baeddec9cd82e8995d8e5b1ec2bdf875b6c
shrlyhe <she@wesleyan.edu>
2018-08-10 11:36:30 -0400

    add services page

---
+++ wp-content/themes/liquorlicenseadvisor/services.php
@@ -0,0 +1,41 @@
+<?php 
+
+/** Template Name: Services Page
+ * description: Service page for LLA
+ */
+
+get_header(); ?>
+
+<div class="row">
+	<div class="col-sm-12 services">
+		<h3 class="blog-post-title">OVERVIEW</h3>
+	</div>
+
+	<div class="col-sm-8 job">
+		<h3 class="blog-post-title">JOB SPECIFIC SERVICES</h3>
+		<h2 id="specific">LAWYERS</h3>
+		<h2 id="specific">LICENSING COORDINATORS</h2>
+		<h2 id="specific">MANAGERS</h2>
+		<h2 id="specific">FRANCHISE DEVELOPERS</h2>
+		<h2 id="specific">LIQUOR STORE OWNERS</h2>
+
+	</div>
+
+
+	<div class="col-sm-8 begin">
+		<h3 class="blog-post-title">Don't Know Where to Begin? Start Here</h3>
+	</div>
+
+</div>
+
+<div class="banner">
+	<h3 class="blog-post-title">CTA BANNER</h3>
+
+
+</div>
+
+
+
+</div> <!-- /.row -->
+
+<?php get_footer(); ?>
