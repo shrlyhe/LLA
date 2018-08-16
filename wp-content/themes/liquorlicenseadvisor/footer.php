141dd600152b30d4b2d2d8d864048ae5805500fc
shrlyhe <she@wesleyan.edu>
2018-08-16 16:57:31 -0400

    add footer

--- wp-content/themes/liquorlicenseadvisor/footer.php
+++ wp-content/themes/liquorlicenseadvisor/footer.php
@@ -1,14 +1,21 @@
-    </div> <!-- /.container -->
+ 
+<div class="container">
+  <div class="row">
 
-		<footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
+  <footer>
+      <div>
+        <ul class="footer">
+          <li class="col-sm-2">ABOUT</li>
+          <li class="col-sm-2">SERVICES</li>
+          <li class="col-sm-2">RESOURCES</li>
+          <li class="col-sm-2">BLOG</li>
+          <li class="col-sm-2">CONTACT</li>
+        </div>
+      </footer>
+    </div>
+  </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<?php wp_footer(); ?> 
-  </body>
-</html>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <?php wp_footer(); ?> 
