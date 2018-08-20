e3f3632c030237fd5e7ba5aacc4733a473329cb4
shrlyhe <she@wesleyan.edu>
2018-08-20 09:15:05 -0500

    fix about page overview

--- wp-content/themes/liquorlicenseadvisor/about.php
+++ wp-content/themes/liquorlicenseadvisor/about.php
@@ -7,46 +7,67 @@
 get_header(); ?>
 
 <div class="container">
-<div class="row">
-	<div class="col-sm-12 about-overview">
-		<h3 class="blog-post-title">OVERVIEW</h3>
+	<div class="row">
+
+
+		<div class="col-sm-12 about-header">
+			<h1>WHAT WE DO</h1>
+		</div>
+
+		<div class="col-sm-8 about-overview">
+			<h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur.</h3>
+		</div>
+
+		<div class="col-sm-4 about-service">
+			<ul data-columns="2">
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+				<h3><li>Service 1</li></h3>
+			</ul>
+
 	</div>
 
-	<div class="col-sm-8 about-twitter">
-		<h3 class="blog-post-title">LIVE TWITTER FEED</h3>
-	</div>
-
-</div>
-
-<br><br>
-
-<div>
-	<h2 class="client-review-title">Our Team</h2>
 	<br><br>
-	<div class="review-column">
-		<h2> PERSON1 </h2>
-		<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
-	</div>
-	<div class="review-column">
-		<h2> PERSON2 </h2>
-		<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
-	</div>
-	<div class="review-column">
-		<h2> PERSON3 </h2>
-		<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
-	</div>
-	<div class="review-column">
-		<h2> PERSON4 </h2>
-		<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
-	</div>
-	<div class="review-column">
-		<h2> PERSON5 </h2>
-		<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
-	</div>
-	<div class="review-column">
-		<h2> PERSON6 </h2>
-		<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
-	</div>
+
+	<div>
+		<h2 class="client-review-title">Our Team</h2>
+		<br><br>
+		<div class="review-column">
+			<h2> PERSON1 </h2>
+			<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
+		</div>
+		<div class="review-column">
+			<h2> PERSON2 </h2>
+			<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
+		</div>
+		<div class="review-column">
+			<h2> PERSON3 </h2>
+			<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
+		</div>
+		<div class="review-column">
+			<h2> PERSON4 </h2>
+			<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
+		</div>
+		<div class="review-column">
+			<h2> PERSON5 </h2>
+			<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
+		</div>
+		<div class="review-column">
+			<h2> PERSON6 </h2>
+			<img id="good-smiley" src="<?php echo get_stylesheet_directory_uri(); ?>/images/good-smiley.png"/>
+		</div>
 
 	</div>
 
--- wp-content/themes/liquorlicenseadvisor/contact.php
+++ wp-content/themes/liquorlicenseadvisor/contact.php
@@ -31,7 +31,7 @@ get_header(); ?>
 
 	<div class="col-sm-4 office-info">
 		<h2>DETAILS</h2>
-		<h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus.</h3>
+		<h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. </h3>
 	</div>
 
 
