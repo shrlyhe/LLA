8b9153d41baf2c50da039c104eb8a7c287b84292
shrlyhe <she@wesleyan.edu>
2018-08-24 15:23:32 -0400

    get rid of button

--- wp-content/themes/liquorlicenseadvisor/searchform.php
+++ wp-content/themes/liquorlicenseadvisor/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<div>
 		<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder=" &#x1F50D; Search" />
-		<input type="submit" id="searchsubmit" value="Search" class="button" />
+<!-- 		<input type="submit" id="searchsubmit" value="Search" class="button" /> -->
 	</div>
 </form>
