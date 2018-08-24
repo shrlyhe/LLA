362c377e1df550e78432eca5ac06578238f64559
shrlyhe <she@wesleyan.edu>
2018-08-24 10:21:08 -0400

    push font

--- wp-content/themes/liquorlicenseadvisor/header.php
+++ wp-content/themes/liquorlicenseadvisor/header.php
@@ -10,11 +10,17 @@
 
 	<title>LLA</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	<link href="<?php echo get_bloginfo( 'template_directory' );?>/site.css" rel="stylesheet">
   <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+  <link href="<?php echo get_bloginfo( 'template_directory' );?>/site.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Lato');
+  </style>
+
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -23,19 +29,19 @@
     </head>
 
     <body>
-    <div class="container">
-    <div class="row">
-       <div class="blog-masthead">
-        <nav class="blog-nav">
-         <div class="container-header">
-          <img id="lla-logo" style="float: left;" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/> 
-          <a class="blog-nav-item active" href="<?php echo get_bloginfo( 'wpurl' );?>">HOME</a>
-          <?php wp_list_pages( '&title_li=&exclude=21,23,25,27,29&sort_column=menu_order' ); ?>
-        </nav>
-      </div> <!-- end container-header -->
-    </div> <!-- end container-masthead -->
+      <div class="container">
+        <div class="row">
+         <div class="blog-masthead">
+          <nav class="blog-nav">
+           <div class="container-header">
+            <img id="lla-logo" style="float: left;" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/> 
+            <a class="blog-nav-item active" href="<?php echo get_bloginfo( 'wpurl' );?>">HOME</a>
+            <?php wp_list_pages( '&title_li=&exclude=21,23,25,27,29&sort_column=menu_order' ); ?>
+          </nav>
+        </div> <!-- end container-header -->
+      </div> <!-- end container-masthead -->
 
-  </div>
+    </div>
   </div>
 </nav>
 </body>
