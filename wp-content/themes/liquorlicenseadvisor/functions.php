051558977922c6d45ccd958b27e9f5709ce5ce21
shrlyhe <she@wesleyan.edu>
2018-08-28 09:56:45 -0400

    fix functions file

--- wp-content/themes/liquorlicenseadvisor/functions.php
+++ wp-content/themes/liquorlicenseadvisor/functions.php
@@ -1,44 +1,18 @@
 <?php
 
+// Add scripts and stylesheets
+function startwordpress_scripts() {
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
+	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
+}
 
-// function fb_change_search_url_rewrite() 
-// {
-//     if ( is_search() && ! empty( $_GET['s'] ) ) 
-//     {
-//         wp_redirect( esc_url( home_url( '/' ) ) . urlencode( get_query_var( 's' ) ) ); 
-//         exit();
-//     }   
-// }
-// add_action( 'template_redirect', 'fb_change_search_url_rewrite' );
+add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
 
+// Add Google Fonts
+function startwordpress_google_fonts() {
+				wp_register_style('Lato', 'https://fonts.googleapis.com/css?family=Lato');
+				wp_enqueue_style( 'Lato');
+		}
 
-
-
-// function isu_search_url( $query ) {
-
-//     $page_id = 38; // This is ID of page with your structure -> http://example.com/mysearch/
-//     $per_page = 10;
-//     $post_type = 'resources'; // I just modify a bit this querry
-
-//     // Now we must edit only query on this one page
-//     if ( !is_admin() && $query->is_main_query() && $query->queried_object->ID == $page_id  ) {
-//         // I like to have additional class if it is special Query like for activity as you can see
-//         add_filter( 'body_class', function( $classes ) {
-//             $classes[] = 'filter-search';
-//             return $classes;
-//         } );
-//         $query->set( 'pagename', '' ); // we reset this one to empty!
-//             $query->set( 'posts_per_page', $per_page ); // set post per page or dont ... :)
-//             $query->set( 'post_type', $post_type ); // we set post type if we need (I need in this case)
-//             // 3 important steps (make sure to do it, and you not on archive page, 
-//             // or just fails if it is archive, use e.g. Query monitor plugin )
-//             $query->is_search = true; // We making WP think it is Search page 
-//             $query->is_page = false; // disable unnecessary WP condition
-//             $query->is_singular = false; // disable unnecessary WP condition
-//         }
-// }
-// add_action( 'pre_get_posts', 'isu_search_url' );
-
-
-
-?>
+add_action('wp_print_styles', 'startwordpress_google_fonts');
--- wp-content/themes/liquorlicenseadvisor/header.php
+++ wp-content/themes/liquorlicenseadvisor/header.php
@@ -9,7 +9,7 @@
 	<meta name="author" content="">
 
 	<title>LLA</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
   <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
   <link href="<?php echo get_bloginfo( 'template_directory' );?>/site.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -17,8 +17,6 @@
   <style>
     @import url('https://fonts.googleapis.com/css?family=Lato');
   </style>
-
-
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
@@ -29,24 +27,23 @@
     </head>
 
     <body>
-    <nav class="navbar navbar-custom">
-      <div class="container">
-       <div class="blog-masthead">
-       <!--  <nav class="blog-nav"> -->
-          <img id="lla-logo" style="float: left;" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/>
-          <div class="header">
+      <nav class="navbar navbar-custom">
+        <div class="container">
+         <div class="blog-masthead">
+           <!--  <nav class="blog-nav"> -->
+           <img id="lla-logo" style="float: left;" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/>
+           <div class="header">
             <ul class="nav navbar-nav">
               <li><a href="<?php echo get_bloginfo( 'wpurl' );?>">HOME</a></li>
               <li><?php wp_list_pages( '&title_li=&exclude=21,23,25,27,29&sort_column=menu_order' ); ?></li>
             </ul>
           </div>
+        </div>
+
       </div>
+    </nav>
 
-  </div>
-  </nav>
-
-
-</body>
-</html>
+  </body>
+  </html>
 
 
