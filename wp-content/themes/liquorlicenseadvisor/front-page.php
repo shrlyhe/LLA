0b0466799b647db161bdbfdf01bbbbbd2be25f3c
shrlyhe <she@wesleyan.edu>
2018-08-29 12:03:27 -0400

    add new content to blog

---
+++ wp-content/themes/liquorlicenseadvisor/front-page.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+<div class="container">
+  <div class="row">
+
+    <div class="col-sm-12 main">
+      <div class="descriptor">
+        <h3 class="blog-post-title">LLA DESCRIPTOR/TAGLINE</h3>
+        <button type="button" class="license-buy">Buy a license</button>
+        <button type="button" class="license-sell">Sell a license</button>
+      </div>
+    </div>
+
+
+
+    <div class="col-sm-6 column-dan">
+      <img id="pic-dan" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pic-dan.jpg"/> 
+    </div>
+
+    <div class="col-sm-6 column-quote">
+      <h1> "QUOTE FROM DAN" </h1><br><br><br>
+      <h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante.</h3>
+    </div>
+
+
+  <div class="col-sm-12">
+    <h2 class="client-review-title">HERE'S WHAT CLIENTS ARE SAYING</h2>
+    <div class="review-column" >
+      <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
+      <h2> REVIEW 1 </h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus.</p>
+    </div>
+    <div class="review-column">
+      <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
+      <h2> REVIEW 2 </h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. </p>
+    </div>
+    <div class="review-column">
+      <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
+      <h2> REVIEW 3 </h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. </p>
+    </div>
+  </div>
+
+</div>
+
+
+
+
+</div> <!-- /.row -->
+
+<?php get_footer(); ?> 
+
+
--- wp-content/themes/liquorlicenseadvisor/index.php
+++ wp-content/themes/liquorlicenseadvisor/index.php
@@ -2,49 +2,18 @@
 <div class="container">
   <div class="row">
 
-    <div class="col-sm-12 main">
-      <div class="descriptor">
-        <h3 class="blog-post-title">LLA DESCRIPTOR/TAGLINE</h3>
-        <button type="button" class="license-buy">Buy a license</button>
-        <button type="button" class="license-sell">Sell a license</button>
-      </div>
-    </div>
+      <div class="col-sm-12" style="float:center; padding: 2%;">
+      <!--  <h2>Beginner SEO Content</h2> -->
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+          </div><!-- entry -->
+        <?php endwhile; ?>
+      <?php endif; ?>
 
-    <div class="col-sm-6 column-dan">
-      <img id="pic-dan" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pic-dan.jpg"/> 
-    </div>
-
-    <div class="col-sm-6 column-quote">
-      <h1> "QUOTE FROM DAN" </h1><br><br><br>
-      <h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante.</h3>
     </div>
 
 
-  <div class="col-sm-12">
-    <h2 class="client-review-title">HERE'S WHAT CLIENTS ARE SAYING</h2>
-    <div class="review-column" >
-      <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
-      <h2> REVIEW 1 </h2>
-      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus.</p>
-    </div>
-    <div class="review-column">
-      <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
-      <h2> REVIEW 2 </h2>
-      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. </p>
-    </div>
-    <div class="review-column">
-      <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
-      <h2> REVIEW 3 </h2>
-      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. </p>
-    </div>
-  </div>
-
-</div>
-
-
-
-
-</div> <!-- /.row -->
+</div> 
 
 <?php get_footer(); ?> 
 
