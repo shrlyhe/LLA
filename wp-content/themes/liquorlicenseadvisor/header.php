1f8e449b91e2822b460bc98d563623dbbd20fc33
shrlyhe <she@wesleyan.edu>
2018-09-10 19:46:56 +0200

    test mobile

--- wp-content/themes/liquorlicenseadvisor/header.php
+++ wp-content/themes/liquorlicenseadvisor/header.php
@@ -4,9 +4,10 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- 	<meta name="viewport" content="width=device-width, initial-scale=1"> -->
 	<meta name="description" content="">
 	<meta name="author" content="">
+
 
 	<title>LLA</title>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
