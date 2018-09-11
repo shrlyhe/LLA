e1daa83a9bd693479500ad64ef0c12fc7e1fc18e
shrlyhe <she@wesleyan.edu>
2018-09-11 12:44:58 +0200

    add client reviews

--- wp-content/themes/liquorlicenseadvisor/front-page.php
+++ wp-content/themes/liquorlicenseadvisor/front-page.php
@@ -32,8 +32,9 @@
           <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
         </div>
 
-        <div class="col-sm-12">
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus.</p>
+        <div class="col-sm-12" style="text-align: left;">
+          <p> "Efficient and Smooth.<br><br> I have worked with many brokers over the years and consider Dan and team to be the best of the best! Not only do they provide quick professional results, they stay with you throughout the process to ensure the deal moves smoothly and that both parties are satisfied. Without question, they have simplified a process that I used to consider stressful and burdensome."</p>
+          <p style="text-align: right;">- Carrie Chadbourne<br>Bloomin' Brands, Inc.</p>
         </div>
       </div>
 
@@ -42,7 +43,8 @@
         <img id="pic-client" src="<?php echo get_stylesheet_directory_uri(); ?>/images/client-pic.jpeg"/> 
       </div>
       <div class="col-sm-12">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec pellentesque ante. In rhoncus, dui consectetur fermentum maximus, neque odio elementum tortor, ut tristique tortor nisl at dui. Aliquam vel ipsum quis sem fermentum tristique. Nam eget molestie risus. Curabitur volutpat ante non auctor efficitur. Pellentesque nisl massa, tempus a egestas et, sagittis vulputate odio. Aenean commodo auctor tempus. </p>
+        <p style="text-align: left;"> "I recently hired Dan Newcomb and Atlantic License Brokers to facilitate what can only be described as an absurdly difficult all alcohol liquor license transfer.  I am not exaggerating when I say that I do not believe that anybody other than Dan and his team could have gotten this transaction completed and especially under the serious time constraints we faced.  Dan never gave up even when I was ready to myself.  If you want to hire somebody who knows the business, knows the players and knows how to get things done, you won't go wrong with Dan and his team at Atlantic." </p>
+         <p style="text-align: right;">- Joanne Audette, Esq.</p>
       </div>
     </div>
 
