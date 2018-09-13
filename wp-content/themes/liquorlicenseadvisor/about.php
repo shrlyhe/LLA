5e7fb068db37617ada5207edb00496aadff80c4a
shrlyhe <she@wesleyan.edu>
2018-09-13 23:11:05 +0200

    split to two sections

--- wp-content/themes/liquorlicenseadvisor/about.php
+++ wp-content/themes/liquorlicenseadvisor/about.php
@@ -7,159 +7,162 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
-	<div class="wrap">
-
-		<div class="col-sm-12 about-header">
-			<h1>WHAT WE DO</h1>
-		</div>
-
-		<div class="col-sm-10 about-overview">
-			<h3> Whether you’re buying or selling a license, Liquor License Advisor (formerly Atlantic License Brokers) is a one-stop shop for individuals who want to learn more about the liquor licensing process for the first time as well as seasoned food and beverage professionals looking for assistance as they pursue licenses. 
-				<br><br>
-
-			After over a decade in the liquor licensing world, we’ve refined our process to better serve a vast array of clients with many different requests. Amongst our most popular services, we routinely offer our extensive knowledge base to attorneys who may not be familiar with the specifics of liquor licensing, work with franchise developers to secure licenses that match their timelines and lease obligations, and offer consultations to corporate food and beverage partners on proper zoning and market considerations before they ever break ground. We also regularly work with licensing coordinators and managers, liquor store owners, and lenders on all of their liquor licensing needs. 
-			<br><br>
-
-			Clients come back to Liquor License Advisor time and time again because in addition to years of experience, they know we also work to troubleshoot problems with unmatched resourcefulness and creativity. At the end of the day, we take pride in our expertise and ability to streamline an often stressful and confusing process into a simple and straightforward one.
-			<br><br>
-
-			To learn more about our general offerings and specialized services for attorneys, licensing coordinators, liquor store owners, franchise developers, food and beverage establishment owners, lenders, and more, <a href="<?php echo get_page_link( get_page_by_title( SERVICES )->ID ); ?>" style="font-weight:bold; color: black;">CLICK HERE</a>.
+		<div class="wrap">
 
 
-			</h3>
-		</div>
+			<div class="col-sm-12 about-header">
+				<h1>WHAT WE DO</h1>
+			</div>
 
-		<div class="col-sm-2 about-service">
-			<ul>
-				<h3><li>Attorneys</li></h3>
-				<h3><li>Licensing Coordinators</li></h3>
-				<h3><li>Managers</li></h3>
-				<h3><li>Franchise Developers</li></h3>
-				<h3><li>Liquor Store Owners</li></h3>
-			</ul>
+			<div class="col-sm-10 about-overview">
+				<h3> Whether you’re buying or selling a license, Liquor License Advisor (formerly Atlantic License Brokers) is a one-stop shop for individuals who want to learn more about the liquor licensing process for the first time as well as seasoned food and beverage professionals looking for assistance as they pursue licenses. 
+					<br><br>
 
-		</div>
+					After over a decade in the liquor licensing world, we’ve refined our process to better serve a vast array of clients with many different requests. Amongst our most popular services, we routinely offer our extensive knowledge base to attorneys who may not be familiar with the specifics of liquor licensing, work with franchise developers to secure licenses that match their timelines and lease obligations, and offer consultations to corporate food and beverage partners on proper zoning and market considerations before they ever break ground. We also regularly work with licensing coordinators and managers, liquor store owners, and lenders on all of their liquor licensing needs. 
+					<br><br>
 
+					Clients come back to Liquor License Advisor time and time again because in addition to years of experience, they know we also work to troubleshoot problems with unmatched resourcefulness and creativity. At the end of the day, we take pride in our expertise and ability to streamline an often stressful and confusing process into a simple and straightforward one.
+					<br><br>
 
-		<!-- PEOPLE SECTION -->
-	<div class="col-sm-12">
-		<h1 class="about-header" id="people">PEOPLE</h1>
-	</div>
-	<div class="about-category col-sm-4">
-		<img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/about-dan.jpg"/>
-		<h3 id="people-name">Dan Newcomb</h3>
-		<h2><i>Founder, Broker, and Advisor</i></h2><br>
-		<h2>Dan is the founder of Liquor License Advisor. He’s advised the nation’s most prominent restaurant, hotel and retail brands for over 15 years, working alongside clients as well as attorneys, lenders, and development teams to deliver results with timeliness and integrity. His work in the world of liquor licensing isn’t limited to brokering, either. Dan readily consults many of the largest food and beverage customers in the country on topics such as market research, real estate zoning, and licensing legislation changes.<br><br>
-
-		In addition to an earned reputation for meeting licensing needs with professional and personalized service, Dan loves tackling complex assignments that involve enthusiasm and thinking innovatively to problem-solve. He’s dialed into everything you need to make licensing simple, and looks forward to helping people think bigger about the future.
-		</h2>	
-	</div>
+					To learn more about our general offerings and specialized services for attorneys, licensing coordinators, liquor store owners, franchise developers, food and beverage establishment owners, lenders, and more, <a href="<?php echo get_page_link( get_page_by_title( SERVICES )->ID ); ?>" style="font-weight:bold; color: black;">CLICK HERE</a>.
 
 
-	<div class="about-category col-sm-4">
-		<img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/about-ben.jpg"/>
-		<h3 id="people-name"><b>Ben Jerrom</b></h3>
-		<h2><i>Partner and Broker</i></h2><br>
-		<h2>Ben brings diverse experiences to the Liquor License Advisor team, making him well-equipped to handle all major aspects of liquor license brokering in addition to being well-versed in the food and beverage industry. <br><br>
+				</h3>
+			</div>
 
-		In fact, Ben worked his way up, serving in restaurants while learning the ins and outs of the industry. Later, he was part of real estate law before joining international law firm Baker & McKenzie LLP. <br><br>
+			<div class="col-sm-2 about-service">
+				<ul>
+					<h3><li>Attorneys</li></h3>
+					<h3><li>Licensing Coordinators</li></h3>
+					<h3><li>Managers</li></h3>
+					<h3><li>Franchise Developers</li></h3>
+					<h3><li>Liquor Store Owners</li></h3>
+				</ul>
 
-		In 2016, Ben joined Liquor License Advisor, and helped expand LLA’s presence in New Jersey and Pennsylvania. He’s especially invested in building relationships with business owners and local attorneys, as well as consulting clients on the value of their licenses to help expedite the process and ensure efficiency and timeliness. Ben’s hands-on work in the food and beverage industry coupled with his time in the legal field give him a unique skill set that makes license transfers seamless for every party involved. </h2>
-
-
-	</div>
-
-	<div class="about-category col-sm-4">
-		<img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/about-becky.jpg"/>
-		<h3 id="people-name"><b>Becky Smith</b></h3>
-		<h2><i>Operations Manager and Executive Assistant</i></h2><br>
-		<h2 >Becky’s skills in managing financial and business operations throughout her career add an extra layer of timeliness, attention to detail, and creative problem-solving to the Liquor License Advisor team. She supervised a closing team of one of the nation’s top 30 lenders, and she supported a managing attorney and owner of a boutique law firm serving Fortune 500 clients in real estate, foreclosure and bankruptcy matters. <br><br>
-
-		At LLA, Becky facilitates successful closing and consulting projects for over 200 clients, and she’s committed to delivering exceptional results to clients and attorneys with value-added service. 
-		</h2>
-	</div>
-
-		<!-- CLIENTS SECTION -->
-		<div class="about-awards col-sm-12">
-			<h1 class="about-header">CLIENTS</h1>
-		</div>
-		<div class="about-category col-sm-4">
-			<h3><b><u>Off Premise Licensing</u></b></h3>
-			<ul>
-				<li>Total Wine</li>
-				<li>Liquor Barn</li>
-				<li>Stop & Shop</li>
-				<li>Roche Bros</li>
-				<li>Star Market</li>
-				<li>Wegman's</li>
-				<li>7-Eleven</li>
-				<li>BJ’s Wholesale Club®</li>
-				<li>Trader Joe’s</li>
-			</ul>
-		</div>
+			</div>
 
 
-		<div class="about-category col-sm-4">
-			<h3><b><u>On Premise Licensing</u><b></h3>
-			<ul>
-				<li>Nordstroms</li>
-				<li>Dave & Buster’s</li>
-				<li>Bar Louie®</li>
-				<li>Chili’s</li>
-				<li>Bertucci’s</li>
-				<li>Morton’sTM Steakhouse</li>
-				<li>Landry's</li>
-				<li>Darden Restaurant Group</li>
-				<li>Bloomin Brands</li>
-				<li>Sbarro</li>
-				<li>Applebee's</li>
-				<li>Legal Seafoods</li>
-				<li>Shake Shack</li>
-				<li>Wahlburgers</li>
-				<li>Lyon's Group</li>
-				<li>Big Night Entertainment</li>
-				<li>Not Your Average Joe's
-				<li>TGI Fridays
-				<li>Smith & Wollensky
-				<li>Uno Chicago Grill
-				<li>American Airlines Admiral's Club
-				<li>Delta Airlines Sky Club
-				<li>United Airlines
-				<li>US Airways
-				<li>HMS Host
-				<li>Boston Uiniversity
-				<li>Renaissance
-				<li>Marriott
-				<li>Hilton
-				<li>Westmont Hotels
-				<li>Pyramid Group
-				<li>APCV Boston Hotel
-				<li>Restoration Hardware
-			</ul>
+			<!-- PEOPLE SECTION -->
+			<div class="col-sm-12">
+				<h1 class="about-header" id="people">PEOPLE</h1>
+			</div>
+			<div class="about-category col-sm-4">
+				<img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/about-dan.jpg"/>
+				<h3 id="people-name">Dan Newcomb</h3>
+				<h2><i>Founder, Broker, and Advisor</i></h2><br>
+				<h2>Dan is the founder of Liquor License Advisor. He’s advised the nation’s most prominent restaurant, hotel and retail brands for over 15 years, working alongside clients as well as attorneys, lenders, and development teams to deliver results with timeliness and integrity. His work in the world of liquor licensing isn’t limited to brokering, either. Dan readily consults many of the largest food and beverage customers in the country on topics such as market research, real estate zoning, and licensing legislation changes.<br><br>
 
-		</div>
-
-		<div class="about-category col-sm-4">
-			<h3><b><u>Liquor License Valuation and Consulting</u><b></h3>
-			<ul>
-				<li>Century Bank</li>
-				<li>Citizens Bank</li>
-				<li>Santander</li>
-				<li>Northern Bank</li>
-				<li>Haverhill Bank
-				<li>Eastern Bank
-				<li>Salem Five
-				<li>Abington Bank
-				<li>Cooperative Bank
-			</ul>
-		</div>
-
-	
-</div>
-</div>
+					In addition to an earned reputation for meeting licensing needs with professional and personalized service, Dan loves tackling complex assignments that involve enthusiasm and thinking innovatively to problem-solve. He’s dialed into everything you need to make licensing simple, and looks forward to helping people think bigger about the future.
+				</h2>	
+			</div>
 
 
-</div>
+			<div class="about-category col-sm-4">
+				<img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/about-ben.jpg"/>
+				<h3 id="people-name"><b>Ben Jerrom</b></h3>
+				<h2><i>Partner and Broker</i></h2><br>
+				<h2>Ben brings diverse experiences to the Liquor License Advisor team, making him well-equipped to handle all major aspects of liquor license brokering in addition to being well-versed in the food and beverage industry. <br><br>
+
+					In fact, Ben worked his way up, serving in restaurants while learning the ins and outs of the industry. Later, he was part of real estate law before joining international law firm Baker & McKenzie LLP. <br><br>
+
+					In 2016, Ben joined Liquor License Advisor, and helped expand LLA’s presence in New Jersey and Pennsylvania. He’s especially invested in building relationships with business owners and local attorneys, as well as consulting clients on the value of their licenses to help expedite the process and ensure efficiency and timeliness. Ben’s hands-on work in the food and beverage industry coupled with his time in the legal field give him a unique skill set that makes license transfers seamless for every party involved. </h2>
 
 
-<?php get_footer(); ?>
+				</div>
+
+				<div class="about-category col-sm-4">
+					<img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/about-becky.jpg"/>
+					<h3 id="people-name"><b>Becky Smith</b></h3>
+					<h2><i>Operations Manager and Executive Assistant</i></h2><br>
+					<h2 >Becky’s skills in managing financial and business operations throughout her career add an extra layer of timeliness, attention to detail, and creative problem-solving to the Liquor License Advisor team. She supervised a closing team of one of the nation’s top 30 lenders, and she supported a managing attorney and owner of a boutique law firm serving Fortune 500 clients in real estate, foreclosure and bankruptcy matters. <br><br>
+
+						At LLA, Becky facilitates successful closing and consulting projects for over 200 clients, and she’s committed to delivering exceptional results to clients and attorneys with value-added service. 
+					</h2>
+				</div>
+
+				<!-- CLIENTS SECTION -->
+				<div class="about-awards col-sm-12">
+					<h1 class="about-header">CLIENTS</h1>
+				</div>
+				<div class="about-category col-sm-4" id="offpremise-licensing">
+					<h3><u>Off Premise Licensing</u></h3>
+					<ul>
+						<li>Total Wine</li>
+						<li>Liquor Barn</li>
+						<li>Stop & Shop</li>
+						<li>Roche Bros</li>
+						<li>Star Market</li>
+						<li>Wegman's</li>
+						<li>7-Eleven</li>
+						<li>BJ’s Wholesale Club®</li>
+						<li>Trader Joe’s</li>
+					</ul>
+				</div>
+
+
+				<div class="about-category col-sm-4" id="onpremise-licensing">
+					<h3><u>On Premise Licensing</u></h3>
+					<ul style="columns: 2;
+					-webkit-columns: 2;
+					-moz-columns: 2;">
+					<li>Nordstroms</li>
+					<li>Dave & Buster’s</li>
+					<li>Bar Louie®</li>
+					<li>Chili’s</li>
+					<li>Bertucci’s</li>
+					<li>Morton’sTM Steakhouse</li>
+					<li>Landry's</li>
+					<li>Darden Restaurant Group</li>
+					<li>Bloomin Brands</li>
+					<li>Sbarro</li>
+					<li>Applebee's</li>
+					<li>Legal Seafoods</li>
+					<li>Shake Shack</li>
+					<li>Wahlburgers</li>
+					<li>Lyon's Group</li>
+					<li>Big Night Entertainment</li>
+					<li>Not Your Average Joe's
+						<li>TGI Fridays
+							<li>Smith & Wollensky
+								<li>Uno Chicago Grill
+									<li>American Airlines Admiral's Club
+										<li>Delta Airlines Sky Club
+											<li>United Airlines
+												<li>US Airways
+													<li>HMS Host
+														<li>Boston Uiniversity
+															<li>Renaissance
+																<li>Marriott
+																	<li>Hilton
+																		<li>Westmont Hotels
+																			<li>Pyramid Group
+																				<li>APCV Boston Hotel
+																					<li>Restoration Hardware
+																					</ul>
+
+																				</div>
+
+																				<div class="about-category col-sm-4" id="valuation-licensing">
+																					<h3><b><u>Liquor License Valuation and Consulting</u><b></h3>
+																					<ul>
+																						<li>Century Bank</li>
+																						<li>Citizens Bank</li>
+																						<li>Santander</li>
+																						<li>Northern Bank</li>
+																						<li>Haverhill Bank
+																							<li>Eastern Bank
+																								<li>Salem Five
+																									<li>Abington Bank
+																										<li>Cooperative Bank
+																										</ul>
+																									</div>
+
+																								</div>
+																							</div>
+
+
+																						</div>
+
+
+
+																						<?php get_footer(); ?>
