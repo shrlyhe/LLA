4a490d7e75093fc48b24bc0ef9d45f08aca2fc91
shrlyhe <she@wesleyan.edu>
2018-09-13 22:47:14 +0200

    click here to see our latest listings

--- wp-content/themes/liquorlicenseadvisor/marketplace.php
+++ wp-content/themes/liquorlicenseadvisor/marketplace.php
@@ -11,6 +11,11 @@ get_header(); ?>
 
 		<div class="wrap">
 
+		<div class="col-sm-12 marketplace-button">
+			<h1><a href="">CLICK HERE TO SEE OUR LATEST LISTINGS</a></h1>
+
+		</div>
+
 			<div class="contact-header">
 				<h1>MARKETPLACE</h1>
 				<h3>Aenean vel elementum leo. Sed sapien nunc, pretium commodo pellentesque congue, volutpat quis elit. Praesent aliquam odio sed nisi commodo, dignissim luctus dui vestibulum. Ut ut enim quis est convallis pulvinar vitae et felis. Proin vulputate enim quis purus placerat maximus. Aliquam bibendum turpis eget augue interdum vulputate. Quisque ornare nunc lacus, quis tristique odio porttitor quis. Curabitur consectetur at ipsum sed placerat.</h3>
@@ -22,7 +27,7 @@ get_header(); ?>
 				<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 				<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.5/iframeResizer.js"></script>
 
-				<iframe id="bbs" src="https://www.bizbuysell.com/brokerdirectory/Profile/ViewAllListings.aspx?J=b&I=35047&m_dmr=1" marginwidth="0" marginheight="0" scrolling="no" style="border-style:none;width:100%;" onload="$(window).scrollTop(0);"></iframe>
+				<iframe id="bbs" src="https://www.bizbuysell.com/brokerdirectory/Profile/ViewAllListings.aspx?J=b&I=35047&m_dmr=1" marginwidth="0" marginheight="0" scrolling="no" style="border-style:none;width:1000px;" onload="$(window).scrollTop(0);"></iframe>
 
 				<script type="text/javascript">
 					try { iFrameResize({},'#bbs'); } catch(e) {}
