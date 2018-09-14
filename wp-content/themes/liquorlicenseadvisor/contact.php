53e59b46e880127c1f6298b83cbdba2a9510946a
shrlyhe <she@wesleyan.edu>
2018-09-14 23:31:37 +0200

    fix contact page

--- wp-content/themes/liquorlicenseadvisor/contact.php
+++ wp-content/themes/liquorlicenseadvisor/contact.php
@@ -16,10 +16,10 @@ get_header(); ?>
 				<h1>CONTACT US</h1>
 			</div>
 
-			<div class="col-sm-12 office-info">
-			<div class="message-box-header">
-			<h2>SEND US A MESSAGE</h2>
-			</div>
+			<div class="col-sm-12 contact-info">
+				<div class="message-box-header">
+					<h2>SEND US A MESSAGE</h2>
+				</div>
 				<h3> If you’re ready to take the next steps in securing or selling your liquor license, or just want more information on our resources and services, we’ve got you covered. Our mission is to deliver the resources you need and results you want in a manner that’s quick and easy--and that starts here.<br><br><b>Email:</b> TK <br><b>Phone Number:</b> (781) 319-9800 <br><b>Address:</b> 1 Snow Road, Suite 3 <br>Marshfield, MA 02050 <br>
 				</h3>
 			</div>
