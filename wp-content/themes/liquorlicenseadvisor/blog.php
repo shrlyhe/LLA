4d94d3747c39857bb5218d3f8f8bb14101779cec
shrlyhe <she@wesleyan.edu>
2018-09-20 20:10:03 -0400

    add blog header

--- wp-content/themes/liquorlicenseadvisor/blog.php
+++ wp-content/themes/liquorlicenseadvisor/blog.php
@@ -8,26 +8,25 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row">
-<!-- 	<div class="wrap"> -->
 
-		<!-- SEARCH SECTION -->
-<!-- 		<div class="col-sm-2 blog-search">
-			<form role="search">
-				<div class="search-control">
-					<?php get_search_form(); ?>
+
+
+		<div class="col-sm-12 blog-main">
+			<div class="blog-section">
+				<div class="blog-section-inner">
+				<h1>BLOG</h1>
+				<h2>How Can I Tell If a Liquor License Is Fake?</h2>
+				<h3>Whether you’re looking into buying or selling a liquor license for the first time or dealing with your millionth sale, there’s always one question in the back of your mind: How can you know if a liquor license is real or fake? We get it: Buying and selling a liquor license can be tough—and often there’s a lot of money on the table, particularly if you’re in a state with a quota.</h3>
+				<a href="https://liquorlicenseadvisor.dream.press/2018/08/29/how-can-i-tell-if-a-liquor-license-is-fake/"><button type="button" class="read-more-button">READ MORE</button></a>
 				</div>
-			</form>
-		</div> -->
+			</div>
+		</div> 
 
-<!-- 		<div class="blog-main">
-		</div> -->
-		<div>
-		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-		</div>
 
 
 		<!-- ARTICLES SECTION -->
-<!-- 		<div class="col-sm-10 blog-articles">
+		<div class="wrap"> 
+		<!-- <div class="col-sm-10 blog-articles">
 			<div class="col-sm-10 articles">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<?php the_content(); ?>
@@ -35,10 +34,10 @@ get_header(); ?>
 				<?php endwhile; ?>
 			<?php endif; ?>
 
-		</div> -->
+		</div>  -->
 
 	</div>
 </div>
-<!-- </div> -->
+</div> 
 
 <?php get_footer(); ?>
