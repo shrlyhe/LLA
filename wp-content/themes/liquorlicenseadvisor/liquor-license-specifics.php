b169ec8fcbf3726d0e4ba4fd5322f46fe536783e
shrlyhe <she@wesleyan.edu>
2018-09-29 13:17:27 -0400

    add new page

---
+++ wp-content/themes/liquorlicenseadvisor/liquor-license-specifics.php
@@ -0,0 +1,54 @@
+<?php 
+
+/** Template Name: Liquor License Specifics Page
+ * description: Liquor License Specifics page for LLA
+ */
+
+get_header(); ?>
+
+<div class="container">
+	<div class="row">
+		<div class="wrap">
+
+			<div class="col-sm-12 marketplace-button">
+				<h1><a href="<?php echo get_page_link(46); ?>">CLICK HERE TO SEE OUR CURRENT LISTINGS</a></h1>
+			</div>
+
+			<div class="col-sm-12 about-resources">
+				<h3> Want to learn more about the liquor licensing process? We've been in this business for years and are happy to share our expertise. If you have questions, feel free to <a style="color:black; font-weight: bold;" href="<?php echo get_page_link(14); ?>">contact us.</a></h3>
+			</div>
+
+			<!-- SEARCH SECTION -->
+			<div class="col-sm-2 search">
+				<h1> CATEGORIES </h1>
+				<a href=#><h2> Liquor License Basics </h2></a>
+				<a href=#><h2> Buying a License </h2></a>
+				<a href=#><h2> Selling a License </h2></a>
+				<a href=#><h2> Liquor License Specifics</h2></a>
+			</div>
+
+		<div class="resources-articles">
+
+			<div class="col-sm-10 articles">
+				<p class="imgp"><a href="https://liquorlicenseadvisor.dream.press/2018/08/30/liquor-license-applications-in-florida-massachusetts-new-jersey-and-pennsylvania/"><img class="articles-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/alcohol-bar-beer-1089932.jpg" data-src="https://cdn10.bostonmagazine.com/wp-content/uploads/2016/12/liquor-licenses-boston-sm.jpg" /></a></p>
+
+				<h2><a href="https://liquorlicenseadvisor.dream.press/2018/08/30/liquor-license-applications-in-florida-massachusetts-new-jersey-and-pennsylvania/" style="color:black;">Liquor License Applications in Florida, Massachusetts, New Jersey, and Pennsylvania</a></h2>
+
+			</div>
+
+				<div class="col-sm-10 articles">
+				<p class="imgp"><a href="https://liquorlicenseadvisor.dream.press/2018/08/30/why-hire-a-liquor-license-broker/"><img class="articles-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/alcohol-bar-beer-1283219.jpg" data-src="https://cdn10.bostonmagazine.com/wp-content/uploads/2016/12/liquor-licenses-boston-sm.jpg" /></a></p>
+
+				<h2><a href="https://liquorlicenseadvisor.dream.press/2018/08/30/why-hire-a-liquor-license-broker/" style="color:black;">Why You Should Hire a Liquor License Broker to Obtain a License</a></h2>
+
+			</div>
+
+		</div>
+	</div>
+</div>
+
+</div>
+</div>
+</div>
+
+<?php get_footer(); ?>
