6c9751eeed01f847f9bb6e6a5c7fc0477f556bff
shrlyhe <she@wesleyan.edu>
2018-09-29 16:54:37 -0400

    add toggle collapse button

--- wp-content/themes/liquorlicenseadvisor/header.php
+++ wp-content/themes/liquorlicenseadvisor/header.php
@@ -29,25 +29,75 @@
 
     <body>
      <div class="container">
-        <div class="row">
-      <nav class="navbar navbar-default">
-        <div class="container">
-        <div class="row">
-         <div class="blog-masthead">
-          <div class="contact-home-header">
-          <h3><i class="fa fa-envelope"></i><a style="color: white;"href="mailto:team@llausa.com">team@llausa.com</a> |<i class="fa fa-phone"></i>781.319.9800</h3>
-           </div> 
+      <div class="row">
 
-           
-           </div>
 
-           <div class="logo-div">
-             <!--  <nav class="blog-nav"> -->
+
+
+        <nav class="navbar navbar-default">
+          <!-- Navbar Header [contains both toggle button and navbar brand] -->
+             <div class="row">
+             <div class="blog-masthead">
+              <div class="contact-home-header">
+                <h3><i class="fa fa-envelope"></i><a style="color: white;"href="mailto:team@llausa.com">team@llausa.com</a> |<i class="fa fa-phone"></i>781.319.9800</h3>
+              </div> 
+
+            </div>
+            </div>
+          <div class="navbar-header">
+            <!-- Toggle Button [handles opening navbar components on mobile screens]-->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#exampleNavComponents" aria-expanded="false">
+              <i class="glyphicon glyphicon-align-justify"></i>
+            </button>
+
+            <!-- Navbar Image -->
+            <div class="logo-div">
+            <a href="<?php echo get_bloginfo( 'wpurl' );?>"><img id="lla-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/></a>
+            </div>
+          </div>
+
+
+          <div class="collapse navbar-collapse" id="exampleNavComponents">
+
+            <!-- Navbar Menu -->
+            <ul class="nav navbar-nav navbar-right header">
+              <li>
+                <?php wp_list_pages( '&title_li=&exclude=21,23,25,27,29,51,53,55,247,254,261,263,266&sort_column=menu_order' ); ?>
+              </li>
+            </ul>
+
+
+        </div>
+        </nav>
+        </div>
+        </div>
+
+
+
+
+
+
+
+<!-- 
+
+
+        <nav class="navbar navbar-default">
+          <div class="container">
+            <div class="row">
+             <div class="blog-masthead">
+              <div class="contact-home-header">
+                <h3><i class="fa fa-envelope"></i><a style="color: white;"href="mailto:team@llausa.com">team@llausa.com</a> |<i class="fa fa-phone"></i>781.319.9800</h3>
+              </div> 
+
+
+            </div>
+
+            <div class="logo-div">
              <a href="<?php echo get_bloginfo( 'wpurl' );?>"><img id="lla-logo"  src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/></a>
            </div>
            <div class="header">
             <ul class="nav navbar-nav">
-              <li><?php wp_list_pages( '&title_li=&exclude=21,23,25,27,29,51,53,55,247,254,261,263,266&sort_column=menu_order' ); ?></li>
+              <li><?php wp_list_pages( '&title_li=&exclude=61,63,65,67,69,51,53,55,247,254,261,263,266&sort_column=menu_order' ); ?></li>
             </ul>
           </div>
         </div>
@@ -56,7 +106,7 @@
       </div>
       
 
-    </nav>
+    </nav> -->
 
   </body>
   </html>
