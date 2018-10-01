4261b030e0f570c69ebcd8285316cb6c0edb131a
Shirley He <she@wesleyan.edu>
2018-10-01 10:30:32 -0400

    Update lenders.php

--- wp-content/themes/liquorlicenseadvisor/lenders.php
+++ wp-content/themes/liquorlicenseadvisor/lenders.php
@@ -21,7 +21,11 @@ get_header(); ?>
 			<h1>LENDER</h1>
 
 
-			<h3> Lenders come to us for our consulting services, where we help them understand the market value of their licenses on an annual basis. We work closely with them to assess license values and identify various valuations. <br><br> Our connections with lenders are mutually beneficial for both the lenders themselves and the clients we help who are securing liquor licenses. </h3>
+			<h3> Lenders count on us for our consulting services and market valuation reports.  We have provided hundreds of reports to loan officers in need of the most current market data to support the underwriting of restaurant loans where the liquor license asset is used as a part of the collateral package.  Each time we are engaged to provide a study, we double check the information to provide the best data available.  Our bank clients appreciate our quick response and data accuracy which helps them make better decisions.
+			<br><br>
+			On occasion, if a lender needs to sell a license to cover the collateral, we provide solid advice and execute an aggressive marketing strategy to bring closure and get the bank paid.  Our team manages the entire project from beginning to end to ensure that a closing occurs, and the lender is protected.
+			<br><br>
+			When you are asked to loan against a liquor license or are updating your file for the auditors, contact our team to get what you need.  All valuation services are customized to meet your needs and priced accordingly.</h3>
 			<button type="button" class="begin-button">NEED HELP? CONNECT WITH LIQUOR LICENSE ADVISOR NOW</button>
 		</div>
 
