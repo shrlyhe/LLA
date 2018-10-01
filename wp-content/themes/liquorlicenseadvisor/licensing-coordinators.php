7c426fed0bc8b09aceea62d6660fd57341903627
Shirley He <she@wesleyan.edu>
2018-10-01 10:21:59 -0400

    Update licensing-coordinators.php

--- wp-content/themes/liquorlicenseadvisor/licensing-coordinators.php
+++ wp-content/themes/liquorlicenseadvisor/licensing-coordinators.php
@@ -18,11 +18,16 @@ get_header(); ?>
 
 		<div class="col-sm-8 services">
 
-			<h1>LICENSING COORDINATOR & MANAGER</h1>
+			<h1>CORPORATE LICENSING MANAGER</h1>
 
 
-			<h3> When licensing coordinators and managers at food and beverage organizations are analyzing stores to assess market value, they come to us for appraisals. We can provide market data, highlight estimated values of other licenses that have sold in the area, and identify pockets in the market that are ripe for buying or selling a license. The information we provide can also help licensing coordinators and managers make decisions when opening and closing establishments. 
- </h3>
+			<h3>Whether you company is a national restaurant chain operating in a quota state or a retail brand operating across the US, you can expect quick, professional results from our team!  We understand the pressure your under and the demands that the CEO, CFO, Operations, Legal and Construction have on your dwindling department.  They want answers yesterday and results today.  As your department’s resources are continuously strapped, the demand for results are increasing.
+			<br><br>  
+			Liquor License Advisor has been assisting corporate clients by providing market values, license availability, brokerage services and strategy for more than 2 decades.  Our systems, processes and vast network of brokers and attorneys provide you with the resources that you need to support your company’s liquor license objectives.  Whether you need data to support a store closing in Massachusetts, or market values to determine a budget on a new location in Pennsylvania, New Jersey or Florida, or a quote for compiling a chain market valuation report for year end, our team will own the project from start to finish!  
+			<br><br>
+			We listen to our clients and understand what they’re up against.  You expect and deserve better communication, a quicker response, more value and a resource that you can trust to get the job done.
+			<br><br>
+			If you want professional help getting things done fast, call Liquor License Advisor today!</h3>
 			<a href="<?php echo get_page_link(14); ?>"><button type="button" class="begin-button" style="color:black">NEED HELP? CONNECT WITH LIQUOR LICENSE ADVISOR NOW</button></a>
 		</div>
 
