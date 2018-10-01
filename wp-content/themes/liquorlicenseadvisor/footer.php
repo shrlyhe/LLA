8b9f8b2d6f4678644998a1568cb96beca36562c2
Shirley He <she@wesleyan.edu>
2018-10-01 12:11:10 -0400

    remove youtube

--- wp-content/themes/liquorlicenseadvisor/footer.php
+++ wp-content/themes/liquorlicenseadvisor/footer.php
@@ -48,7 +48,6 @@
           <a href="https://twitter.com/LiquorAdvisor" target="_blank" class="fa fa-twitter"></a>
           <a href="https://www.facebook.com/Liquor-License-Advisor-282320729178768/" target="_blank" class="fa fa-facebook"></a>
           <a href="https://www.linkedin.com/company/atlantic-license-brokers/" target="_blank" class="fa fa-linkedin"></a>
-          <a href="https://www.youtube.com/" target="_blank" class="fa fa-youtube"></a>
           <h3 class="rights-reserved">2018 Liquor License Advisor USA All rights reserved<br>This website is designed by <a href="https://www.roguesunday.com/" target="_blank">Rogue Sunday</a></h3>
         </div>
 
