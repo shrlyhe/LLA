6bfbeb7195f0643615e60d3df23b8695cf1c9bec
shrlyhe <she@wesleyan.edu>
2018-09-30 23:39:54 -0400

    add new blog posts

--- wp-content/themes/liquorlicenseadvisor/blog.php
+++ wp-content/themes/liquorlicenseadvisor/blog.php
@@ -53,7 +53,7 @@ get_header(); ?>
 		<div class="about-category col-sm-4">
 			<a href="https://liquorlicenseadvisor.dream.press/2018/08/29/how-can-i-tell-if-a-liquor-license-is-fake/"><img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/alcohol-bar-beer-1283219.jpg"/> </a>
 			<a href="https://liquorlicenseadvisor.dream.press/2018/08/29/how-can-i-tell-if-a-liquor-license-is-fake/"><h3 id="people-name" style="color: black;">How Can I Tell If a Liquor License Is Fake?</h3><br></a>
-			<h2>Whether you’re looking into buying or selling a liquor license for the first time or dealing with your millionth sale, there’s always one question in the back of your mind: How can you know if a liquor license is real or fake?</h2>	
+			<h2>Whether you’re looking into buying or selling a liquor license for the first time or dealing with your millionth sale, there’s always one question...</h2>	
 		</div>
 
 
@@ -68,6 +68,36 @@ get_header(); ?>
 			<a href="https://liquorlicenseadvisor.dream.press/2018/08/29/most-people-dont-know-that-liquor-license-brokers-can-help-with-these-3-services/"><img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/architecture-bar-beer-751046.jpg"/></a> 
 			<a href="https://liquorlicenseadvisor.dream.press/2018/08/29/most-people-dont-know-that-liquor-license-brokers-can-help-with-these-3-services/"><h3 id="people-name" style="color: black;"><b>Most People Don’t Know That Liquor License Brokers Can Help With These 3 Services</b></h3></a>
 			<h2>Liquor license brokers are experts for a reason. We know the ins and outs of liquor licensing and can save you time, money, and headaches.  </h2>
+
+		</div>
+
+		<!-- NEW -->
+
+				<div class="about-category col-sm-4">
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/5-tips-for-getting-and-keeping-a-liquor-license-in-a-state-with-a-quota/"><img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/alcoholic-beverages-bar-beverage-613037.jpg"/></a> 
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/5-tips-for-getting-and-keeping-a-liquor-license-in-a-state-with-a-quota/"><h3 id="people-name" style="color: black;"><b>5 Tips for Getting (and Keeping) A Liquor License in a State With a Quota</b></h3></a>
+			<h2>A business can’t legally sell alcoholic beverages to consumers without a valid liquor license. </h2>
+
+		</div>
+
+				<div class="about-category col-sm-4">
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/how-to-control-your-liquor-cost-and-boost-profitability/"><img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/adult-alcohol-alcohol-bottles-681846.jpg"/></a> 
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/how-to-control-your-liquor-cost-and-boost-profitability/"><h3 id="people-name" style="color: black;"><b>How to Control Your Liquor Cost—And Boost Profitability</b></h3></a><br>
+			<h2>Most restaurateurs know how to control liquor cost, but thanks to the fact that some of the “controls” can be tricky and time-consuming. </h2>
+
+		</div>
+
+				<div class="about-category col-sm-4">
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/how-bar-owners-can-protect-against-financial-losses-from-staff/"><img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/alcoholic-beverage-bar-beer-1269025.jpg"/></a> 
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/how-bar-owners-can-protect-against-financial-losses-from-staff/"><h3 id="people-name" style="color: black;"><b>How Bar Owners Can Protect Against Financial Losses From Staff</b></h3></a>
+			<h2>Most restaurateurs know how to control liquor cost, but thanks to the fact that some of the “controls” can be tricky and time-consuming, they get skipped, causing costs to skyrocket.  </h2>
+
+		</div>
+
+				<div class="about-category col-sm-4">
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/what-does-public-need-for-a-liquor-license-mean/"><img id="about-team-pics" src="<?php echo get_stylesheet_directory_uri(); ?>/images/stock-pics/alcohol-architecture-bar-331107.jpg"/></a> 
+			<a href="https://liquorlicenseadvisor.dream.press/2018/10/01/what-does-public-need-for-a-liquor-license-mean/" style="color: black;"><h3 id="people-name" style="color: black;"><b>What Does “Public Need” for a Liquor License Mean?</b></h3></a>
+			<h2>Did you know that sometimes, in order to get a liquor license for a location not previously licensed, you have to demonstrate “public need?”</h2>
 
 		</div>
 
