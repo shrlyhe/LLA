41df0d11e7ac232167f9b69225a19648e270f7b4
shrlyhe <she@wesleyan.edu>
2018-12-03 20:48:29 -0500

    add buttons

--- wp-content/themes/liquorlicenseadvisor/sell-a-license-new.php
+++ wp-content/themes/liquorlicenseadvisor/sell-a-license-new.php
@@ -15,6 +15,13 @@ get_header(); ?>
 				<h1>SELL A LICENSE</h1>
 				<h3>Some sort of paragraph that welcomes the visitor to the website and makes them feel comfortable then explains the options available on this page (inquiry form and ready to sell form).</h3>
 
+				<div class="descriptor">
+					<a href="<?php echo get_page_link(46); ?>" style="color:white;text-decoration: none;"><button type="button" class="license-buy">BUY A LICENSE</button></a>
+
+					<a href="<?php echo get_page_link(14); ?>" style="color:white;text-decoration: none;"><button type="button" class="license-sell">SELL A LICENSE</button></a>
+					
+				</div>
+
 
 
 				<br><br><h3>For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3>
