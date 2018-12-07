2f8e5819a46126941887e01eefd9eeb42701f970
shrlyhe <she@wesleyan.edu>
2018-12-06 21:42:12 -0500

    add new inquiry form page

---
+++ wp-content/themes/inquiry.php
@@ -0,0 +1,40 @@
+<?php 
+
+/** Template Name: Inquiry Form
+ * description: Inquiry form for "Sell a License" page
+ */
+
+get_header(); ?>
+
+<div class="container">
+	<div class="row">
+
+		<div class="wrap">
+
+			<div class="contact-header">
+				<h1>INQUIRY FORM</h1>
+				<br>
+				<h3>Some sort of paragraph that welcomes the visitor to the website and makes them feel comfortable then explains the options available on this page (inquiry form and ready to sell form).</h3>
+
+				<div class="sell-button-div">
+
+					
+					<div class="contact-overall">
+						<div class="col-sm-12 message-box">
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+								<?php the_content(); ?>
+							<?php endwhile; ?>
+						<?php endif; ?>
+					</div>
+				</div> 
+
+				<h3>For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3>
+			</div>
+			
+		</div>
+
+	</div>
+</div>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/liquorlicenseadvisor/sell.php
+++ wp-content/themes/liquorlicenseadvisor/sell.php
@@ -22,14 +22,9 @@ get_header(); ?>
 
 					<a href="<?php echo get_page_link(14); ?>" style="color:white;text-decoration: none;"><button type="button" class="upload">UPLOAD LICENSE</button></a>
 
-						<h3>For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3>
+					<h3>For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3>
 				</div>
-
-
-
-		
-			
-			
+				
 			</div>
 
 		</div>
