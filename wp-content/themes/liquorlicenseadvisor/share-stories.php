d1e16406f80e908b3fe49c7d1f246c2df929a261
shrlyhe <she@wesleyan.edu>
2018-12-08 17:24:12 -0500

    fix share stories page

--- wp-content/themes/liquorlicenseadvisor/share-stories.php
+++ wp-content/themes/liquorlicenseadvisor/share-stories.php
@@ -12,55 +12,25 @@ get_header(); ?>
 
 		<div class="wrap">
 			
-    <div class="col-sm-12 client-testimonial-title">
-      <h2>CLIENT TESTIMONIALS</h2>
-    </div>
-
-
-      <div class="col-sm-12">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <?php the_content(); ?>
-        <?php endwhile; ?>
-      <?php endif; ?>
-
-    </div>
-
-	<!-- 		<div class="col-sm-12 contact-info">
-				<h3> If you’re ready to take the next steps in securing or selling your liquor license, or just want more information on our resources and services, we’ve got you covered. Our mission is to deliver the resources you need and results you want in a manner that’s quick and easy--and that starts here.<br><br><b>
-				</h3>
-			</div> -->
-
-	<!-- 		<div class="contact-box-mobile">
-			<h3>1 Snow Rd #3 <br>
-					Marshfield MA 02050<br>
-					781.319.9800<br>
-					<a style="color:black;" href="mailto:team@llausa.com">team@llausa.com</a><br><br><br>
-				</h3>
-			</div>
- -->
-<!-- 			<div class="contact-overall">
-				<div class="col-sm-12 message-box">
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-								<?php the_content(); ?>
-						<?php endwhile; ?>
-					<?php endif; ?>
-				</div>
-			</div>  
-
-			<div class="contact-box-info">
-				<h3>1 Snow Rd #3 <br>
-					Marshfield MA 02050<br>
-					781.319.9800<br>
-					<a style="color:black;" href="mailto:team@llausa.com">team@llausa.com</a><br>
-				</h3>
+			<div class="col-sm-12 client-testimonial-title">
+				<br><br>
+				<h2>CLIENT TESTIMONIALS</h2>
 			</div>
 
-			</div> -->
 
+			<div class="col-sm-12">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
 
 		</div>
+
 	</div>
+</div>
+</div>
 
 
 
-	<?php get_footer(); ?>
+
+<?php get_footer(); ?>
