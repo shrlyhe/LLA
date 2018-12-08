7afd2b0d045c3cf66c0bc5e631498b73feb2453e
shrlyhe <she@wesleyan.edu>
2018-12-08 14:40:34 -0500

    fix upload form

--- wp-content/themes/liquorlicenseadvisor/upload.php
+++ wp-content/themes/liquorlicenseadvisor/upload.php
@@ -19,7 +19,7 @@ get_header(); ?>
 
 
 				<div class="contact-overall">
-					<div class="col-sm-12 message-box">
+					<div class="col-sm-12 message-box-upload-license">
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 							<?php the_content(); ?>
 						<?php endwhile; ?>
