53dc0e9caeddd0535e7b1b4d699d90e530f3fd66
shrlyhe <she@wesleyan.edu>
2018-12-09 14:47:49 -0500

    add new menu

--- wp-content/themes/liquorlicenseadvisor/header.php
+++ wp-content/themes/liquorlicenseadvisor/header.php
@@ -15,6 +15,10 @@
   <link href="<?php echo get_bloginfo( 'template_directory' );?>/site.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"> -->
+
   <style>
     @import url('https://fonts.googleapis.com/css?family=Lato');
   </style>
@@ -31,50 +35,60 @@
      <div class="container">
       <div class="row">
 
-
-
-
         <nav class="navbar navbar-default">
           <!-- Navbar Header [contains both toggle button and navbar brand] -->
-             <div class="row">
-             <div class="blog-masthead">
-              <div class="contact-home-header">
-                <h3><i class="fa fa-envelope"></i><a style="color: white;"href="mailto:team@llausa.com">team@llausa.com</a> |<i class="fa fa-phone"></i>781.319.9800</h3>
-              </div> 
+          <div class="row">
+           <div class="blog-masthead">
+            <div class="contact-home-header">
+              <h3><i class="fa fa-envelope"></i><a style="color: white;"href="mailto:team@llausa.com">team@llausa.com</a> |<i class="fa fa-phone"></i>781.319.9800</h3>
+            </div> 
 
-            </div>
-            </div>
-          <div class="navbar-header">
-            <!-- Toggle Button [handles opening navbar components on mobile screens]-->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#exampleNavComponents" aria-expanded="false">
-              <i class="glyphicon glyphicon-align-justify"></i>
-            </button>
-
-            <!-- Navbar Image -->
-            <div class="logo-div">
-            <a href="<?php echo get_bloginfo( 'wpurl' );?>"><img id="lla-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/></a>
-            </div>
           </div>
-
-
-          <div class="collapse navbar-collapse" id="exampleNavComponents">
-
-            <!-- Navbar Menu -->
-            <ul class="nav navbar-nav navbar-right header">
-              <li>
-                <?php wp_list_pages( '&title_li=&exclude=21,23,25,27,29,51,53,55,247,254,261,263,266,335,328,323&sort_column=menu_order' ); ?>
-              </li>
-             <!--  sell a license: #328
-              share your stories: #323 -->
-            </ul>
-
-
         </div>
-        </nav>
-        </div>
+        <div class="navbar-header">
+          <!-- Toggle Button [handles opening navbar components on mobile screens]-->
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#exampleNavComponents" aria-expanded="false">
+            <i class="glyphicon glyphicon-align-justify"></i>
+          </button>
+
+          <!-- Navbar Image -->
+          <div class="logo-div">
+            <a href="<?php echo get_bloginfo( 'wpurl' );?>"><img id="lla-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LLA-logo.png"/></a>
+          </div>
         </div>
 
-  </body>
-  </html>
+
+        <div class="collapse navbar-collapse" id="exampleNavComponents">
+          <!-- Navbar Menu -->
+          <ul class="nav navbar-nav navbar-right header">
+            <li class="dropdown">
+             <a href="<?php echo get_page_link(6); ?>">ABOUT<span class="caret"></span></a>
+             <ul class="dropdown-menu">
+               <li><a href="<?php echo get_page_link(323); ?>">Share Your Story</a>
+               </ul>
+             </li>
+             <li><a href="<?php echo get_page_link(8); ?>">SERVICES</a></li>
+             <li class="dropdown"><a href="<?php echo get_page_link(46); ?>">MARKETPLACE<span class="caret"></span></a>
+               <ul class="dropdown-menu">
+                 <li><a href="<?php echo get_page_link(328); ?>">Sell a License</a></li>
+               </ul>
+             </li>
+             <li><a href="<?php echo get_page_link(10); ?>">RESOURCES</a></li>
+             <li><a href="<?php echo get_page_link(12); ?>">BLOG</a></li>
+             <li><a href="<?php echo get_page_link(14); ?>">CONTACT</a></li>
+           </ul>
+         </li>
+       </ul>
+
+
+     </div>
+   </nav>
+
+
+ </div>
+</div>
+
+</body>
+</html>
 
 
