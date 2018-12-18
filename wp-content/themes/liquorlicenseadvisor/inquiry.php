a3a77287f8ef9c1ef1c99b38b58391596123211f
shrlyhe <she@wesleyan.edu>
2018-12-17 20:25:02 -0500

    fix font

--- wp-content/themes/liquorlicenseadvisor/inquiry.php
+++ wp-content/themes/liquorlicenseadvisor/inquiry.php
@@ -14,7 +14,7 @@ get_header(); ?>
 			<div class="contact-header">
 				<h1>INQUIRY FORM</h1>
 				<br>
-				<h3>Some sort of paragraph that welcomes the visitor to the website and makes them feel comfortable then explains the options available on this page (inquiry form and ready to sell form).</h3><br><br>
+				<h3 style="font-size: 18px;">Some sort of paragraph that welcomes the visitor to the website and makes them feel comfortable then explains the options available on this page (inquiry form and ready to sell form).</h3><br><br>
 
 
 
@@ -29,7 +29,7 @@ get_header(); ?>
 		</div>
 
 		<div>
-		<h3 style="font-size: 16px;">For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3><br><br><br>
+		<h3 style="font-size: 18px;">For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3><br><br><br>
 		</div>
 
 	</div>
