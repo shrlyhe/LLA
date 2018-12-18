f2c950f0fe872d497f4142ed4c438000007791ea
shrlyhe <she@wesleyan.edu>
2018-12-17 20:22:17 -0500

    fix font

--- wp-content/themes/liquorlicenseadvisor/upload.php
+++ wp-content/themes/liquorlicenseadvisor/upload.php
@@ -29,7 +29,7 @@ get_header(); ?>
 		</div>
 
 		<div>
-		<h3 style="font-size: 16px;">For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3><br><br><br>
+		<h3 style="font-size: 18px;">For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3><br><br><br>
 		</div>
 
 	</div>
