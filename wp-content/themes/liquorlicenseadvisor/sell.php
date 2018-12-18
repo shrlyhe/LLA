efd66a0e91eafaee7a4d71e51c57ca52e02c1de1
shrlyhe <she@wesleyan.edu>
2018-12-17 19:45:13 -0500

    fix sell license page

--- wp-content/themes/liquorlicenseadvisor/sell.php
+++ wp-content/themes/liquorlicenseadvisor/sell.php
@@ -14,15 +14,15 @@ get_header(); ?>
 			<div class="contact-header">
 				<h1>SELL A LICENSE</h1>
 				<br>
-				<h3>Some sort of paragraph that welcomes the visitor to the website and makes them feel comfortable then explains the options available on this page (inquiry form and ready to sell form).</h3>
+				<h3 style="font-size: 18px;">Some sort of paragraph that welcomes the visitor to the website and makes them feel comfortable then explains the options available on this page (inquiry form and ready to sell form).</h3>
 
 				<div class="sell-button-div">
 
 					<a href="<?php echo get_page_link(335); ?>" style="color:white;text-decoration: none;"><button type="button" class="inquire">INQUIRE</button></a>
 
-					<a href="<?php echo get_page_link(341); ?>" style="color:white;text-decoration: none;"><button type="button" class="upload">UPLOAD LICENSE</button></a>
+					<a href="<?php echo get_page_link(341); ?>" style="color:white;text-decoration: none;"><button type="button" class="upload">UPLOAD LICENSE</button></a><br><br>
 
-					<h3>For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3>
+					<h3 style="font-size: 18px; text-align: left;">For questions or concerns please call us at 781.319.9800 or use our <a style="font-weight:bold; color: black;" href="<?php echo get_page_link(14); ?>">online contact form</a>.</h3>
 				</div>
 
 			</div>
